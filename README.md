00a81d5459abf6d63d3a30f266a51076025bddf3
Spyware <gabrielluisdm13@gmail.com>
2022-11-12 18:27:16 -0300

    commit all

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # hangman-game
+
+Um joguinho da forca feito em python
+
+## Windows
+> baixe o main.exe
+
+## Linux
+```
+apt install git python3
+git clone https://github.com/Spyware0/hangman-game
+cd hangman-game
+python main.py
+```
+
+- Acesse meu [Whatsapp](http://wa.me/559885267746) para tirar dúvidas ou enviar críticas
+
+_dá uma dedada na estrelinha :)_
---
+++ main.py
@@ -0,0 +1,46 @@
+from desenho import desenhol
+
+print('\x1b[2J\x1b[1;1H')
+tentativas = c = 0
+letra = ''
+letras_erradas = []
+letras_certas = []
+
+palavra = input("Digite o nome da palavra: ").strip().lower()
+dica = input("Digite a dica: ").strip()
+
+while (tentativas != 6):
+    print('\x1b[2J\x1b[1;1H')
+    print('Dica:',dica)
+    print(desenhol[tentativas])
+    print(f'Letras erradas: {letras_erradas}')
+    print(f'Letras certas: {letras_certas}')
+    letra = input("\nLetra: ").lower()[0]
+    if (letra in palavra):
+        if (letra not in letras_certas):
+            letras_certas.append(letra)
+        for i in palavra:
+            if (i in letras_certas):
+                print(f"\033[4m{i}\033[m", end=' ')
+                c += 1
+            else:
+                print('_', end=' ')
+        if (c == len(palavra)):
+            print('\x1b[2J\x1b[1;1H')
+            print("\nDesafio cumprido!")
+            break
+        c = 0
+        
+        
+    else:
+        print(f'"{letra}" não existe nessa palavra')
+        if (letra not in letras_erradas):
+            letras_erradas.append(letra)
+            tentativas += 1
+        print('Tentativas restantes:', 6 - tentativas)
+    
+    input('\nenter para continuar')
+
+print(desenhol[tentativas])
+print(f'Tentativas esgotadas!\nPalavra certa: {palavra}')
+input('\nenter para sair')
